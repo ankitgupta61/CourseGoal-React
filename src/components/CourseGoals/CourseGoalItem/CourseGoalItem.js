e21c4da1d3ff7f815841e21200869e5107b4506f
ankitgupta61 <ankit.work3093@gmail.com>
2022-10-26 20:21:40 +0530

    all files added

---
+++ src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./CourseGoalItem.css";
+
+const CourseGoalItem = (props) => {
+  // const [deleteText, setDeleteText] = useState('');
+
+  const deleteHandler = () => {
+    // setDeleteText('(Deleted!)');
+    props.onDelete(props.id);
+  };
+
+  return (
+    <li className="goal-item">
+      {props.children}
+      <span onClick={deleteHandler}>X</span>
+    </li>
+  );
+};
+
+export default CourseGoalItem;
